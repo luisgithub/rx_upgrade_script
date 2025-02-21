abfcea240314277e76d99814e76bc08fa2dfe8d1
Luis Garcia <lugar.blanco@gmail.com>
2025-02-20 17:45:11 -0700

    - se genera el sql para hacer el update de producto

--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,6 @@
 
 from src.main.utils.logger_config import setup_logger
-from src.main.rx_service import fetch_rx_recordds
+from src.main.rx_service import fetch_rx_records, get_id_tipo_producto_by_description, get_id_producto_by_description_and_clinic
 from src.main.rx_service import close
 
 
@@ -11,10 +11,11 @@ def main():
     page_size = 100
     offset = 0
     while True:
-        records = fetch_rx_recordds(page_size, offset)
+        records = fetch_rx_records(page_size, offset)
         if records:
             logger.info(f"{len(records)} records fetched from DB..")
             for record in records:
+                logger.info(f"record: {record}")
                 update_field(record)
             offset += page_size
         else:
@@ -25,10 +26,16 @@ def main():
    
 
 def update_field(record):
+    rx_id = record.get("id")
+    clinica_id = record.get("clinica")
     lente_desc = record.get("lente_descripcion")
     tipo_lente_desc = record.get("tipo_lente_descripcion")
     if not lente_desc.isdigit() and not tipo_lente_desc.isdigit():
-        logger.info(f"lente: {lente_desc}")
+        tipo_lente_id = get_id_tipo_producto_by_description(tipo_lente_desc)
+        
+        producto_id = get_id_producto_by_description_and_clinic(lente_desc, clinica_id)
+    
+        logger.info(f"UPDATE rx SET lente_rx = {producto_id},  tipo_lente_rx = {tipo_lente_id} WHERE id = {rx_id};")
     
 
 
--- src/main/rx_service.py
+++ src/main/rx_service.py
@@ -9,7 +9,7 @@ pool_manager = DatabaseConnection()
 def get_connection():
     return pool_manager.get_connection()
 
-def fetch_rx_recordds(page_size, offset):
+def fetch_rx_records(page_size, offset):
     
     try:
         # Get a connection from the pool
@@ -32,30 +32,32 @@ def fetch_rx_recordds(page_size, offset):
     # finally:
     #     pool_manager.close_pool() #close the pool when done.
 
-def get_id_tipo_producto_by_description(tipo):
+def get_id_tipo_producto_by_description(tipo_desc):
     Dict = {
-        '1':'Bifocal',
-        '2':'Lente de Conctacto',
-        '3':'Progresivo',
-        '4':'Solo Cerca',
-        '5':'Solo Lejos',
-        '6':'Vision Sencilla'
+        'Bifocal':'1',
+        'Lente de Conctacto':'2',
+        'Progresivo':'3',
+        'Solo Cerca':'4',
+        'Solo Lejos':'5',
+        'Vision Sencilla':'6',
     }
-    return Dict[tipo]
+    return Dict[tipo_desc]
 
-def get_id_producto_by_description_and_clinic(desc):
+def get_id_producto_by_description_and_clinic(producto_descripcion, clinica):
     try:
         connection = get_connection()
         if(connection):
             cursor = connection.cursor()
-            query = """SELECT id FROM producto WHERE descripcion = ?;"""
-            cursor.execute(query, (desc,))
+            logger.info(f"Parametros: {clinica}")
+            query = "SELECT id FROM producto p WHERE p.clinica = %s AND p.descripcion = %s "
+            params = (int(clinica), producto_descripcion)
+            cursor.execute(query, params)
             result = cursor.fetchone()
             if result is None:
                 return None
             if cursor.fetchone() is not None:  # Check if there's another row
                 raise Exception("Query returned more than one result.  Expected single result.")
-            return result
+            return result[0]
     except psycopg2.Error as e:  # Handle database errors
         logger.error(f"Error while fetching product data : {e}")
         return None  
